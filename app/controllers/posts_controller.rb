51ded16fd26b1e9f1a6a70782441a1bf3ca06e04
fukadashigeru <sakusaku721@gmail.com>
2020-09-21 22:33:09 +0900

    Jobでもレコードを作成する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
+    PostCreateJob.perform_later()
     @post = Post.new(post_params)
 
     respond_to do |format|
